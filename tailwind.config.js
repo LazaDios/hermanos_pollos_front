645cacab65fba3ad9615b3ceddf6b414dd16fe64
Lázaro Marín <48573908+LazaDios@users.noreply.github.com>
2023-05-23 21:45:50 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,39 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        
+        mytheme: {
+
+          "primary": "#F20519",
+
+          "secondary": "#D926AA",
+
+          "accent": "#1FB2A5",
+
+          "neutral": "#191D24",
+
+          "base-100": "#2A303C",
+
+          "info": "#3ABFF8",
+
+          "success": "#05C7F2",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
 }
 
